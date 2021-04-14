c4d8afe2016e090f2d65954721bdfff1d799aaee
JuanF2019 <juan.martinez27@u.icesi.edu.co>
2021-04-13 19:02:59 -0500

    Add basic GUI scheme including data viewer an graphics tabs

--- HSA/UserInterface/MainWindow.Designer.cs
+++ HSA/UserInterface/MainWindow.Designer.cs
@@ -29,20 +29,69 @@ namespace HSA.UserInterface
         /// </summary>
         private void InitializeComponent()
         {
+            this.mainWindowTabs = new System.Windows.Forms.TabControl();
+            this.dataViewTab = new System.Windows.Forms.TabPage();
+            this.graphicsTab = new System.Windows.Forms.TabPage();
+            this.mainWindowTabs.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // mainWindowTabs
+            // 
+            this.mainWindowTabs.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.mainWindowTabs.Controls.Add(this.dataViewTab);
+            this.mainWindowTabs.Controls.Add(this.graphicsTab);
+            this.mainWindowTabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainWindowTabs.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.mainWindowTabs.ItemSize = new System.Drawing.Size(40, 100);
+            this.mainWindowTabs.Location = new System.Drawing.Point(0, 0);
+            this.mainWindowTabs.Multiline = true;
+            this.mainWindowTabs.Name = "mainWindowTabs";
+            this.mainWindowTabs.SelectedIndex = 0;
+            this.mainWindowTabs.Size = new System.Drawing.Size(1008, 561);
+            this.mainWindowTabs.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.mainWindowTabs.TabIndex = 0;
+            // 
+            // dataViewTab
+            // 
+            this.dataViewTab.Location = new System.Drawing.Point(104, 4);
+            this.dataViewTab.Name = "dataViewTab";
+            this.dataViewTab.Padding = new System.Windows.Forms.Padding(3);
+            this.dataViewTab.Size = new System.Drawing.Size(900, 553);
+            this.dataViewTab.TabIndex = 0;
+            this.dataViewTab.Text = "Data Viewer";
+            this.dataViewTab.UseVisualStyleBackColor = true;
+            // 
+            // graphicsTab
+            // 
+            this.graphicsTab.Location = new System.Drawing.Point(104, 4);
+            this.graphicsTab.Name = "graphicsTab";
+            this.graphicsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.graphicsTab.Size = new System.Drawing.Size(900, 553);
+            this.graphicsTab.TabIndex = 1;
+            this.graphicsTab.Text = "Graphics";
+            this.graphicsTab.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1008, 561);
+            this.Controls.Add(this.mainWindowTabs);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimumSize = new System.Drawing.Size(1024, 600);
             this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HSA";
+            this.mainWindowTabs.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl mainWindowTabs;
+        private System.Windows.Forms.TabPage dataViewTab;
+        private System.Windows.Forms.TabPage graphicsTab;
     }
 }
 
--- HSA/UserInterface/MainWindow.cs
+++ HSA/UserInterface/MainWindow.cs
@@ -15,6 +15,44 @@ namespace HSA.UserInterface
         public MainWindow()
         {
             InitializeComponent();
+
+            mainWindowTabs.DrawItem += new DrawItemEventHandler(mainWindowTabs_DrawItem);
+        }
+
+        //Allows vertical tabs, not possible within tab control properties
+        //https://docs.microsoft.com/en-us/dotnet/desktop/winforms/controls/how-to-display-side-aligned-tabs-with-tabcontrol?view=netframeworkdesktop-4.8&redirectedfrom=MSDN
+        private void mainWindowTabs_DrawItem(Object sender, System.Windows.Forms.DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush _textBrush;
+
+            // Get the item from the collection.
+            TabPage _tabPage = mainWindowTabs.TabPages[e.Index];
+
+            // Get the real bounds for the tab rectangle.
+            Rectangle _tabBounds = mainWindowTabs.GetTabRect(e.Index);
+
+            if (e.State == DrawItemState.Selected)
+            {
+
+                // Draw a different background color, and don't paint a focus rectangle.
+                _textBrush = new SolidBrush(Color.Black);
+                g.FillRectangle(Brushes.Gray, e.Bounds);
+            }
+            else
+            {
+                _textBrush = new System.Drawing.SolidBrush(e.ForeColor);
+                e.DrawBackground();
+            }
+
+            // Use our own font.
+            Font _tabFont = new Font("Segoe UI", 12.0f, FontStyle.Bold, GraphicsUnit.Pixel);
+
+            // Draw string. Center the text.
+            StringFormat _stringFlags = new StringFormat();
+            _stringFlags.Alignment = StringAlignment.Center;
+            _stringFlags.LineAlignment = StringAlignment.Center;
+            g.DrawString(_tabPage.Text, _tabFont, _textBrush, _tabBounds, new StringFormat(_stringFlags));
         }
     }
 }
