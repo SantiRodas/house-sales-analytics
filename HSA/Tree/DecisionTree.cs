25cc6fbaf53946de05fe48eff2fe289cacad4830
Santiago <santiago.rodas1@correo.icesi.edu.co>
2021-05-14 11:46:14 -0500

    We implement the node class of the decision tree and propose the Tree package relationships

---
+++ HSA/Tree/DecisionTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSA.Tree
+{
+    class DecisionTree
+    {
+
+        // ----------------------------------------------------------------------------------------------------
+
+        // Information of the root
+
+        private Node root;
+
+        public Node Root
+        {
+            get { return root; }
+            set { root = value; }
+        }
+
+        // ----------------------------------------------------------------------------------------------------
+
+    }
+}
---
+++ HSA/Tree/Node.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSA.Tree
+{
+    class Node
+    {
+
+        // ----------------------------------------------------------------------------------------------------
+
+        // Information of the name
+
+        private String name;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // ----------------------------------------------------------------------------------------------------
+
+        // Information of the edge
+
+        private String edge;
+
+        public String Edge
+        {
+            get { return edge; }
+            set { edge = value; }
+        }
+
+        // ----------------------------------------------------------------------------------------------------
+
+        // Information of the attribute
+
+        private Attribute nodeAttribute;
+
+        public Attribute Attribute
+        {
+            get { return nodeAttribute; }
+            set { nodeAttribute = value; }
+        }
+
+        // ----------------------------------------------------------------------------------------------------
+
+        // Information of the child nodes
+
+        private List<Node> childNodes;
+
+        public List<Node> ChildNodes
+        {
+            get { return childNodes; }
+            set { childNodes = value; }
+        }
+
+        // ----------------------------------------------------------------------------------------------------
+
+        // Information of the table index
+
+        private int tableIndex;
+
+        public int TableIndex
+        {
+            get { return tableIndex; }
+            set { tableIndex = value; }
+        }
+
+        // ----------------------------------------------------------------------------------------------------
+
+        // Information of the leaf
+
+        private bool isLeaf;
+
+        public bool IsLeaf
+        {
+            get { return isLeaf; }
+            set { isLeaf = value; }
+        }
+
+        // ----------------------------------------------------------------------------------------------------
+
+        // Constructor method
+
+        public Node(string name, string edge, Attribute nodeAttribute, int tableIndex, bool isLeaf)
+        {
+            Name = name;
+            Edge = edge;
+            Attribute = nodeAttribute;
+            ChildNodes = new List<Node>();
+            TableIndex = tableIndex;
+            IsLeaf = isLeaf;
+        }
+
+        // ----------------------------------------------------------------------------------------------------
+
+    }
+}
